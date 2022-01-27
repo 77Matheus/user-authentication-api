e8b1439a09adf087cedd005ea9aff37dbd88f8c0
77Matheus <matheusramosoliveira388@gmail.com>
2022-01-27 12:43:53 -0300

    primeiro commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response, NextFunction} from 'express'
+import statusRoute from './routes/status.route'
+import usersRoute from './routes/users.route'
+
+const app = express()
+
+//configuração de aplicação
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+// configuração de rotas
+app.use(usersRoute)
+app.use(statusRoute)
+
+// inicialização do servidor
+app.listen(3000, () => {
+  console.log('Aplicação executando na porta 3000!')
+})
